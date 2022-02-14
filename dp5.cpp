c606c0181ccff7e24e71319b0257213e9079f862
neeharika49 <83670414+neeharika49@users.noreply.github.com>
2022-02-14 22:19:38 +0530

    Unbounded KnapSack

--- dp5.cpp
+++ dp5.cpp
@@ -1,4 +1,4 @@
-Given a knapsack weight W and a set of n items with certain value vali and weight wti, we need to calculate the maximum amount that could make up this quantity exactly. This is different from classical Knapsack problem, here we are allowed to use unlimited number of instances of an item.
+/*Given a knapsack weight W and a set of n items with certain value vali and weight wti, we need to calculate the maximum amount that could make up this quantity exactly. This is different from classical Knapsack problem, here we are allowed to use unlimited number of instances of an item.
 Examples: 
 
 Input : W = 100
@@ -17,7 +17,7 @@ Input : W = 8
        wt[]  = {1, 3, 4, 5}       
 Output : 110 
 We get maximum value with one unit of
-weight 5 and one unit of weight 3.
+weight 5 and one unit of weight 3.*/
 CODE:
 BOTTOM-UP APPROACH
 #include <iostream>
@@ -30,7 +30,7 @@ int knapsack(int wt[],int val[],int W,int n,int dp[][100])
         return 0;
     }
    
-    if(wt[n-1]<W)
+    if(wt[n-1]<=W)
     {
         return dp[n][W]=max(val[n-1]+knapsack(wt,val,W-wt[n-1],n,dp),knapsack(wt,val,W,n-1,dp));
     }
@@ -61,3 +61,30 @@ for(int i=0;i<n;i++)
 }
    cout<<knapsack(weights,values,W,n,dp);
 }
+//TOP-DOWN
+int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        
+        vector<vector<int>>dp(N+1,vector<int>(W+1));
+        
+        for(int i=0;i<=N;i++)
+    {
+        for(int j=0;j<=W;j++)
+        {
+            if(i==0|| j==0)
+            {
+                dp[i][j]=0;
+            }
+           else if(wt[i-1]<=j)
+            {
+                dp[i][j]=max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
+            }
+            else
+            {
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+       return dp[N][W]; 
+    }
